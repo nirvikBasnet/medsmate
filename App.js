0d2be70c0f9b5b9380b37cddaf40afaff239c2e3
nirvikBasnet <nirvik41@gmail.com>
2020-06-06 14:12:02 +1000

    Add button logic update

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import  React, {useState} from 'react';
-import { StyleSheet,View, Text, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet,View, Text, ScrollView, FlatList, TouchableOpacity, Alert } from 'react-native';
 import { Button} from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Header from './components/header';
+import MedsItems from './components/medsItem';
+import AddMeds from './components/addMeds';
 
 
 
 
 export default function App() {
-  const [todos,setTodo] = useState([
+  const [meds,setMeds] = useState([
     {text: 'Paracytamol', key:'1'},
     {text: 'Flexon', key:'2'},
     {text: 'Sytrane', key:'3'}
 
   ])
+
+  const submitHandler = (text) => {
+    if(text.length > 3){
+    setMeds((prevMeds)=>{
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevMeds
+      ];
+
+    });
+  }
+  else{
+    Alert.alert('OPPS!','Meds Must Be More Then 3 Char', [
+      {text:'Understood'}
+    ]);
+
+  }
+  }
+
+  const pressHandler = (key) => {
+    setMeds((prevMeds)=>{
+      return prevMeds.filter(med=>med.key !=key);
+    })
+  }
 
 
   
@@ -22,12 +48,17 @@ export default function App() {
     <View style={styles.container}>
       <Header/>
       <View style={styles.content}>
-        {/* todo form */}
+        <AddMeds submitHandler={submitHandler}/>
         <View style={styles.list}>
           <FlatList
-          data={todos}
+          data={meds}
           renderItem={({ item })=>(
-            <Text>{ item.text }</Text>
+            <MedsItems 
+            item ={ item }
+            pressHandler={pressHandler}
+            
+            />
+
           )}
 
           />
@@ -44,7 +75,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container :{
     flex : 1,
-    backgroundColor: 'orange',
+  
 
    
   
---
+++ components/addMeds.js
@@ -0,0 +1,35 @@
+import  React, {useState} from 'react';
+import { StyleSheet,View, Text,TextInput, ScrollView, FlatList, TouchableOpacity, Button } from 'react-native';
+
+export default function addMeds({submitHandler}){
+
+const [text,setText]=useState('');
+
+const changeHandler =(val)=>{
+    setText(val);
+
+}
+    return (
+        <View>
+            <TextInput style={styles.input}
+            placeholder = 'New Meds ...'
+            onChangeText = {changeHandler}
+            
+            />
+            <Button title='Add Med' color='coral' onPress={()=>submitHandler(text)}/>
+                
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    input :{
+        marginBottom:10,
+        paddingHorizontal:8,
+        paddingVertical:6,
+        borderBottomWidth:1,
+        borderBottomColor:'#ddd'
+
+    }
+});
